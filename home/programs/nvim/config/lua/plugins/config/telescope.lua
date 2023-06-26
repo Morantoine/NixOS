6f45b537bbc731ae34a6b577db3b14768d047bf7
Morantoine <71721545+Morantoine@users.noreply.github.com>
2023-06-26 21:40:57 +0000

    Small nvim fix

--- home/programs/nvim/config/lua/plugins/config/telescope.lua
+++ home/programs/nvim/config/lua/plugins/config/telescope.lua
@@ -3,11 +3,6 @@ if not telescope_status then
 	return
 end
 
-local lens_status, lens = pcall(require, "session-lens")
-if not lens_status then
-	return
-end
-
 local sessions_status, sessions = pcall(require, "auto-session")
 if not sessions_status then
 	return
@@ -21,15 +16,14 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 -- vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", {})
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-vim.keymap.set("n", "<leader>ss", lens.search_session, {})
+vim.keymap.set("n", "<leader>ss", require("auto-session.session-lens").search_session, {})
 vim.keymap.set("n", "<leader>sg", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", {})
 
-lens.setup({
-	-- theme_conf = { border = false },
-	theme_conf = { winblend = 0 },
-})
-
 sessions.setup({
+	session_lens = {
+		theme_conf = { winblend = 0 },
+		load_on_setup = true,
+	},
 	-- don't create a session automatically
 	auto_session_create_enabled = false,
 })
