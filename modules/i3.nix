8af37410b06ec2ab0cad423005d2502f143b8cda
Morantoine <71721545+Morantoine@users.noreply.github.com>
2023-06-27 21:53:24 +0200

    Fixed powermenu and i3bar

--- modules/i3.nix
+++ modules/i3.nix
@@ -20,17 +20,18 @@
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
-        rofi          # application launcher, the same as dmenu
-	      dunst         # notification daemon
-        i3blocks      # status bar
-        i3lock        # default i3 screen locker
-        i3status      # provide information to i3bar
-        i3-gaps       # i3 with gaps
-        picom         # transparency and shadows
-        feh           # set wallpaper
-        acpi          # battery information
-        arandr        # screen layout manager
-        xbindkeys     # bind keys to commands
+			  imagemagick          # for blurring the lock screen
+        rofi             # application launcher, the same as dmenu
+	      dunst            # notification daemon
+        i3blocks         # status bar
+        i3lock           # default i3 screen locker
+        i3status         # provide information to i3bar
+        i3-gaps          # i3 with gaps
+        picom            # transparency and shadows
+        feh              # set wallpaper
+        acpi             # battery information
+        arandr           # screen layout manager
+        xbindkeys        # bind keys to commands
         xorg.xbacklight  # control screen brightness
      ];
     };
