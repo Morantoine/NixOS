11386465020690ac485475db9d55b8ff15f6f79f
Morantoine <antoine.moran@protonmail.com>
2024-12-26 11:29:48 +0100

    obsolete gtk config updated

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,34 +1,81 @@
-{ pkgs, config, ... }:
-
 {
-  gtk = {
-    enable = true;
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+  let
+    gradiencePreset = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/GradienceTeam/Community/next/official/catppuccin-mocha.json";
+      hash = "sha256-4/RVQF6irDueswEXWtmn2CCmyN7VQtQPrDAeg45cTPk=";
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
+    gradienceBuild = pkgs.stdenv.mkDerivation {
+      name = "gradience-build";
+      phases = [
+        "buildPhase"
+        "installPhase"
+      ];
+      nativeBuildInputs = [ pkgs.gradience ];
+      buildPhase = ''
+        shopt -s nullglob
+        export HOME=$TMPDIR
+        mkdir -p $HOME/.config/presets
+        gradience-cli apply -p ${gradiencePreset} --gtk both
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp -r .config/gtk-4.0 $out/
+        cp -r .config/gtk-3.0 $out/
+      '';
     };
-  };
 
-  qt = {
-    enable = true;
-    platformTheme.name = "adwaita";
-    style.package = pkgs.adwaita-qt;
-    style.name = "adwaita-dark";
-  };
+  in
 
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
+  {
+    gtk = {
+      enable = true;
+      font.name = "Inter:medium";
+      catppuccin = {
+        enable = false;
+        icon.enable = true;
+      };
+      theme.name = "adw-gtk3-dark";
+      theme.package = pkgs.adw-gtk3;
+
+      gtk3 = {
+        extraCss = builtins.readFile "${gradienceBuild}/gtk-3.0/gtk.css";
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+          color-scheme = "prefer-dark";
+        };
+      };
+      gtk4 = {
+        extraCss = builtins.readFile "${gradienceBuild}/gtk-4.0/gtk.css";
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+          color-scheme = "prefer-dark";
+        };
+      };
     };
-  };
 
-  home.pointerCursor = {
-    name = "phinger-cursors";
-    package = pkgs.phinger-cursors;
-    size = 24;
-    gtk.enable = true;
-  };
+    catppuccin.pointerCursor.enable = true;
+    home.pointerCursor = {
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
+    };
 
-}
+    qt = {
+      enable = true;
+      platformTheme.name = "adwaita";
+      style.package = pkgs.adwaita-qt;
+      style.name = "adwaita-dark";
+    };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  }
